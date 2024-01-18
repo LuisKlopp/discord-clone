34014294e5e880edcb552b3a04c8dff37a7d42f7
LuisKlopp <fbgus333@gmail.com>
2024-01-18 17:06:20 +0900

    Feat: 다크모드토글 버튼 생성

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/\bmode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }

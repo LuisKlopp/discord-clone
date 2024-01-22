cc84bc573a49f50b3fad63f508ad18ecbfe24403
LuisKlopp <fbgus333@gmail.com>
2024-01-22 23:21:59 +0900

    Feat: Dialog css 스타일링

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,22 @@
 "use clinet";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 export const InitialModal = () => {
-  return <div>This will be a modal!</div>;
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="text-center text-zinc">
+          Customize your server
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };

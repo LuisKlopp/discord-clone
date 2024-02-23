c5bca84fd581c6bdd2a17b6a8d45cb9dd13fda65
LuisKlopp <fbgus333@gmail.com>
2024-02-23 21:45:31 +0900

    Feat: use chat socket 훅 생성

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -12,6 +12,10 @@ import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
+  useRouter,
+  useParams,
+} from "next/navigation";
+import {
   Edit,
   FileIcon,
   ShieldAlert,
@@ -76,6 +80,18 @@ export const ChatItem = ({
   const [isEditing, setIsEditing] =
     useState(false);
   const { onOpen } = useModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(
+      `/servers/${params?.serverId}/conversations/${member.id}`,
+    );
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -152,7 +168,10 @@ export const ChatItem = ({
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
-        <div className="cursor-pointer hover:drop-shadow-md transition">
+        <div
+          onClick={onMemberClick}
+          className="cursor-pointer hover:drop-shadow-md transition"
+        >
           <UserAvatar
             src={member.profile.imageUrl}
           />
@@ -160,7 +179,10 @@ export const ChatItem = ({
         <div className="flex flex-col w-full">
           <div className="flex items-center gap-x-2">
             <div className="flex items-center">
-              <p className="font-semibold text-sm hover:underline cursor-pointer">
+              <p
+                onClick={onMemberClick}
+                className="font-semibold text-sm hover:underline cursor-pointer"
+              >
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>
